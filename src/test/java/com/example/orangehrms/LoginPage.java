700b66d8949ad741d70542fac952da0fb0528c85
Shubham Jagtap <shubh.jagtap14@gmail.com>
2025-03-05 19:46:33 +0530

    This is commit at 5th march.

---
+++ src/test/java/com/example/orangehrms/LoginPage.java
@@ -0,0 +1,12 @@
+package com.example.orangehrms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+// page_url = https://www.jetbrains.com/
+public class MainPage {
+
+    
+}
