614e43bf11b24f0bd7064f5e6e15a72b9eff28bc
Shubham Jagtap <shubh.jagtap14@gmail.com>
2025-03-05 19:46:49 +0530

    This is commit at 5th march.

--- src/test/java/com/example/orangehrms/LoginPage.java
+++ src/test/java/com/example/orangehrms/LoginPage.java
@@ -1,12 +1,31 @@
 package com.example.orangehrms;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 // page_url = https://www.jetbrains.com/
-public class MainPage {
+public class LoginPage {
 
-    
+    WebDriver driver;
+
+    LoginPage(WebDriver driver){
+
+        this.driver=driver;
+    }
+
+    By user_name = By.xpath("//input[@name='username']");
+    By pass_word = By.xpath("//input[@type='password']");
+    By login_button = By.xpath("//button[@type='submit']");
+
+    public void setUser_name(String username) {
+        driver.findElement(user_name).sendKeys(username);
+    }
+
+    public void setPass_word(String password) {
+        driver.findElement(pass_word).sendKeys(password);
+    }
+
+    public void click_Login_button() {
+        driver.findElement(login_button).click();
+    }
 }
--- src/test/java/com/example/orangehrms/LoginPageTest.java
+++ src/test/java/com/example/orangehrms/LoginPageTest.java
@@ -1,64 +1,37 @@
 package com.example.orangehrms;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
-
-import static org.testng.Assert.*;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.time.Duration;
 
-public class MainPageTest {
+public class LoginPageTest {
     private WebDriver driver;
     private LoginPage mainPage;
 
-    @BeforeMethod
+    @BeforeClass
     public void setUp() {
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.jetbrains.com/");
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
-        mainPage = new LoginPage(driver);
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown() {
         driver.quit();
     }
 
     @Test
-    public void search() {
-        mainPage.searchButton.click();
+    public void login() {
 
-        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
-        searchField.sendKeys("Selenium");
-
-        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
-        submitButton.click();
-
-        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
-        assertEquals(searchPageField.getAttribute("value"), "Selenium");
-    }
-
-    @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
-
-        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
-        assertTrue(menuPopup.isDisplayed());
-    }
-
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
-
-        WebElement productsList = driver.findElement(By.id("products-page"));
-        assertTrue(productsList.isDisplayed());
-        assertEquals(driver.getTitle(), "All Developer Tools and Products by JetBrains");
+        mainPage = new LoginPage(driver);
+        mainPage.setUser_name("Admin");
+        mainPage.setPass_word("admin123");
+        mainPage.click_Login_button();
+        Assert.assertEquals(driver.getTitle(), "OrangeHRM");
     }
 }
